cb41afc8e20169278859bbcfa0fb9d1c07898ef7
Endorocket <43682886+Endorocket@users.noreply.github.com>
2019-07-18 17:06:27 +0200

    pre thymeleaf config

--- src/main/java/pl/insert/config/AppConfig.java
+++ src/main/java/pl/insert/config/AppConfig.java
@@ -1,17 +1,19 @@
 package pl.insert.config;
 
 import org.hibernate.cfg.beanvalidation.BeanValidationIntegrator;
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
 import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
-import org.springframework.transaction.support.TransactionTemplate;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import javax.persistence.ValidationMode;
 
@@ -19,7 +21,14 @@ import javax.persistence.ValidationMode;
 @EnableWebMvc
 @EnableTransactionManagement
 @ComponentScan("pl.insert")
-public class AppConfig {
+public class AppConfig implements WebMvcConfigurer, ApplicationContextAware {
+
+    private ApplicationContext applicationContext;
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        this.applicationContext = applicationContext;
+    }
 
     @Bean
     public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
@@ -35,11 +44,5 @@ public class AppConfig {
         JpaTransactionManager transactionManager = new JpaTransactionManager(entityManagerFactory().getObject());
         transactionManager.setRollbackOnCommitFailure(true);
         return transactionManager;
-    }
-
-    // opcjonalnie
-    @Bean
-    public TransactionTemplate transactionTemplate(PlatformTransactionManager transactionManager) {
-        return new TransactionTemplate(transactionManager);
     }
 }
---
+++ src/main/java/pl/insert/controllers/EmployeeController.java
@@ -0,0 +1,31 @@
+package pl.insert.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import pl.insert.data.Employee;
+import pl.insert.services.EmployeeService;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/employees")
+public class EmployeeController {
+
+    private final EmployeeService employeeService;
+
+    @Autowired
+    public EmployeeController(EmployeeService employeeService) {
+        this.employeeService = employeeService;
+    }
+
+    @GetMapping
+    public String getEmployees(Model model) {
+        List<Employee> employees = employeeService.getEmployeeList();
+        model.addAttribute("employees",employees);
+
+        return "employees/list-employees.html";
+    }
+}
